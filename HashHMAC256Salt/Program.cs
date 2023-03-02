561dcc6218250753b3e844936a2a261928594a20
José Vicente <c_jose.vicente@hapvida.com.br>
2023-03-02 16:03:22 -0300

    refector code with C# 11 ♻️

--- HashHMAC256Salt/Program.cs
+++ HashHMAC256Salt/Program.cs
@@ -4,39 +4,32 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1
+
+var passwordResult = CreatePasswordHash("1234A!@#").Result;
+
+Console.WriteLine("hash: " + passwordResult.passwordHash);
+Console.WriteLine("salt: " + passwordResult.passwordSalt);
+
+var confirm = VerifyPasswordHash("1234A!@#", passwordResult.passwordHash, passwordResult.passwordSalt).Result;
+
+Console.WriteLine(confirm);
+
+static async Task<(string passwordSalt, string passwordHash)> CreatePasswordHash(string password)
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            var passwordResult = CreatePasswordHash("1234A!@#").Result;
+    using var hmac = new HMACSHA256();
+    return (Convert.ToBase64String(hmac.Key),
+        Convert.ToBase64String(await hmac.ComputeHashAsync(
+            new MemoryStream(Encoding.ASCII.GetBytes(password)
+            ))));
+}
 
-            Console.WriteLine("hash: " + passwordResult.passwordHash);
-            Console.WriteLine("salt: " + passwordResult.passwordSalt);
+static async Task<bool> VerifyPasswordHash(string password, string storedHash, string storedSalt)
+{
+    using var hmac = new HMACSHA256(Convert.FromBase64String(storedSalt));
+    var computedHash = Convert.ToBase64String(
+        await hmac.ComputeHashAsync(new MemoryStream(Encoding.ASCII.GetBytes(password))));
 
-            var confirm = VerifyPasswordHash("1234A!@#", passwordResult.passwordHash, passwordResult.passwordSalt).Result;
+    Console.WriteLine(computedHash);
 
-            Console.WriteLine(confirm);
-        }
-
-        private static async Task<(string passwordSalt, string passwordHash)> CreatePasswordHash(string password)
-        {
-            using var hmac = new HMACSHA256();
-            return (Convert.ToBase64String(hmac.Key),
-                Convert.ToBase64String(await hmac.ComputeHashAsync(
-                    new MemoryStream(Encoding.ASCII.GetBytes(password)
-                    ))));
-        }
-
-        private static async Task<bool> VerifyPasswordHash(string password, string storedHash, string storedSalt)
-        {
-            using var hmac = new HMACSHA256(Convert.FromBase64String(storedSalt));
-            var computedHash = Convert.ToBase64String(await hmac.ComputeHashAsync(new MemoryStream(Encoding.ASCII.GetBytes(password))));
-
-            Console.WriteLine(computedHash);
-
-            return storedHash.Equals(computedHash);
-        }
-    }
+    return storedHash.Equals(computedHash);
 }
